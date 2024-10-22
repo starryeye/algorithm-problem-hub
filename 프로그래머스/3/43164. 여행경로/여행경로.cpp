e4ab3f9a75900311e837898160ad604bd398acd1
starryeye <33487061+starryeye@users.noreply.github.com>
2024-10-23 00:10:36 +0900

    [level 3] Title: 여행경로, Time: 40.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,5 +1,68 @@
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+void dfs(string airport, vector<string>& path, map<string, vector<string>>& routes, vector<string>& answer, int ticketNum) {
+    
+    // 현재 경로에 공항 추가
+    path.push_back(airport);
+
+    // 모든 티켓을 사용했으면 결과 검토
+    if (path.size() == ticketNum + 1) {
+        if (answer.empty() || path < answer) { // 각원소를 비교하며 사전순으로 빠른지 비교
+            answer = path;
+        }
+        path.pop_back();
+        return;
+    }
+
+    // 현재 공항에서 갈 수 있는 다음 공항 탐색
+    for (int i = 0; i < routes[airport].size(); i++) {
+        
+        string next = routes[airport][i];
+        
+        // 임시로 제거 (항공권 사용)
+        routes[airport].erase(routes[airport].begin() + i); // i 번째 원소 삭제
+        
+        dfs(next, path, routes, answer, ticketNum);
+        
+        // 임시로 제거한 티켓 복원 백트래킹(복원)
+        routes[airport].insert(routes[airport].begin() + i, next); // i 번째에 원소 추가
+    }
+
+    path.pop_back();
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    
+    vector<string> answer;
+    
+    // 출발지 사전순 정렬됨 -> map
+    map<string, vector<string>> routes; // 출발지, 목적지리스트
+    
+    for (auto& ticket : tickets) {
+        routes[ticket[0]].push_back(ticket[1]);
+    }
+    
+    // 목적지리스트 사전순 정렬, less 가 디폴트
+    for (auto& route : routes) {
+        sort(route.second.begin(), route.second.end(), less<string>());
+    }
+
+    vector<string> path;
+    
+    dfs("ICN", path, routes, answer, tickets.size());
+    
+    return answer;
+}
+
+
+/*
+#include <string>
+#include <vector>
 #include <unordered_map>
 #include <algorithm>
 
@@ -34,4 +97,4 @@ vector<string> solution(vector<vector<string>> tickets) {
     reverse(answer.begin(), answer.end());
     
     return answer;
-}
+} */
